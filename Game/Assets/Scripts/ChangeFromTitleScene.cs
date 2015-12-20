ff8ad12c97146de2c7d30b5758ca34c76eebb3d6
Photoperiod <Wolfsenshi@gmail.com>
2015-12-20 13:44:13 -0800

    Added Title Scene Button Script
    Also added a second scene with some assets on it. Doesn't do anything
    currently. Still need to setup the quads and everything properly.

---
+++ Game/Assets/Scripts/ChangeFromTitleScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeFromTitleScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeScene(string sceneName)
+	{
+		Application.LoadLevel (sceneName);
+	}
+
+	public void ChangeScene(int index)
+	{
+		Application.LoadLevel (index);
+	}
+}
